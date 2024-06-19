10e493e3273c1df407cb92657c22bf6479d4e102
franciscojrlopes <fralopes@yahoo.com.br>
2024-06-19 11:40:26 -0300

    Create main.py

---
+++ python_work/PPZ/modulo_2/main.py
@@ -0,0 +1,11 @@
+# main.py
+
+import meu_modulo
+
+nome = "João"
+print(meu_modulo.saudacao(nome))  # Output: Olá, João!
+
+resultado = meu_modulo.soma(5, 3)
+print(resultado)  # Output: 8
+
+print(meu_modulo.PI)  # Output: 3.14159
